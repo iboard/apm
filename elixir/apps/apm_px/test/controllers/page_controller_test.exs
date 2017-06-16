bf4e732aa5ef8115f9ff2751da06bdbfdb421ddf
Andreas Altendorfer <andreas@altendorfer.at>
2017-06-16 16:14:12 +0200

    Add controller test with cookies

--- elixir/apps/apm_px/test/controllers/page_controller_test.exs
+++ elixir/apps/apm_px/test/controllers/page_controller_test.exs
@@ -1,7 +1,8 @@
 defmodule ApmPx.PageControllerTest do
+  require Logger
   use ApmPx.ConnCase
 
-  test "GET /", %{conn: conn} do
+  test "GET / prints a welcome message", %{conn: conn} do
     conn = get conn, "/"
     assert html_response(conn, 200) =~ "Welcome to APM"
   end
@@ -10,4 +11,15 @@ defmodule ApmPx.PageControllerTest do
     conn = get conn, "/"
     assert html_response(conn, 200) =~ "You&#39;re not logged in"
   end
+
+  test "GET / when logged in", %{conn: conn} do
+    session = 
+      conn
+      |> put_resp_cookie("user", "some user")
+      |> put_resp_cookie("role", "some role")
+      |> get( "/" )
+    assert html_response(session, 200) =~ "You&#39;re logged in"
+  end
+
+
 end
--- elixir/apps/apm_px/test/integration/hound_basic_test.exs
+++ elixir/apps/apm_px/test/integration/hound_basic_test.exs
@@ -7,37 +7,41 @@ defmodule ApmPx.HoundBasicTests do
   @logged_in_message  "You're logged in as 'hound user' in the role of a 'developer'"
   @logged_out_message "You're not logged in!"
 
-  @tag :hound
-  test "Simple Login without authentication (by now, we trust our users)" do
+  describe "E2E" do
 
-    # Given
-    navigate_to("http://localhost:4000")
-    element = find_element(:name, "user")
+    @tag :hound
+    test "Simple Login without authentication (by now, we trust our users)" do
 
-    # When
-    fill_field(element, "hound user")
-    submit_element(element)
+      # Given
+      navigate_to("http://localhost:4000")
+      element = find_element(:name, "user")
 
-    # Then
-    assert visible_text({:id, "login-state"}) == @logged_in_message
+      # When
+      fill_field(element, "hound user")
+      submit_element(element)
+
+      # Then
+      assert visible_text({:id, "login-state"}) == @logged_in_message
+    end
+
+
+    @tag :hound
+    test "Log out clears user and role cookie" do
+
+      # Given (logged with user and role)
+      navigate_to("http://localhost:4000")
+      element = find_element(:name, "user")
+      fill_field(element, "hound user")
+      submit_element(element)
+      assert visible_text({:id, "login-state"}) == @logged_in_message
+
+      # When
+      logout = find_element(:id, "logout")
+      click(logout)
+
+      # Then
+      assert visible_text({:id, "login-state"}) == @logged_out_message
+    end
+
   end
-
-  @tag :hound
-  test "Log out clears user and role cookie" do
-
-    # Given (logged with user and role)
-    navigate_to("http://localhost:4000")
-    element = find_element(:name, "user")
-    fill_field(element, "hound user")
-    submit_element(element)
-    assert visible_text({:id, "login-state"}) == @logged_in_message
-
-    # When
-    logout = find_element(:id, "logout")
-    click(logout)
-
-    # Then
-    assert visible_text({:id, "login-state"}) == @logged_out_message
-  end
-
 end
